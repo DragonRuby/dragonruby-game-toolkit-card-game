19263afadc4d5ae86a5bbc1f296add62aa138f32
Matthew <matthewlf98@gmail.com>
2020-09-17 11:24:11 -0500

    Added a file for UI example Broke the UI down into separate sections in the code

--- app/main.rb
+++ app/main.rb
@@ -1,26 +1,36 @@
 def ui args
-  args.state.resource ||= 3
-  args.state.health ||= 100
+  #Discard information
+  args.state.discard_x ||= 800
+  args.state.discard_y ||= 100
   args.state.discard_count ||= 0
-  discard_count_x ||= 800
-  discard_count_y ||= 250
+  args.state.discard_count_x ||= 800
+  args.state.discard_count_y ||= 250
   args.state.discard_label_alpha ||= 0
+  discard   = [ args.state.discard_x, args.state.discard_y, 128, 128, 'sprites/hexagon-black.png' ]
+  discard_label = [ args.state.discard_count_x, args.state.discard_count_y, "Discarded: #{args.state.discardCount}", 0, 0, 0, args.state.discard_label_alpha ]
+  args.outputs.labels << discard_label
+  args.outputs.sprites << discard
+
+  #Energy information (resource is a WIP name)
+  args.state.resource ||= 3
+  args.outputs.labels << [ 25, 650, "Energy: #{args.state.resource}" ]
+
+  #Player information
+  args.state.health ||= 100
+  args.outputs.labels << [ 580, 500, "Health: #{args.state.health}" ]
+
+  #Cards information
   red_x ||= 200
   red_y ||= 100
   green_x ||= 500
   green_y ||= 100
-  discard_x ||= 800
-  discard_y ||= 100
-  args.outputs.labels << [ 580, 500, "Health: #{args.state.health}" ]
-  args.outputs.labels << [ 25, 650, "Energy: #{args.state.resource}" ]
   red_card   = [ red_x,     red_y,     128, 128, 'sprites/hexagon-red.png' ]
   green_card = [ green_x,   green_y,   128, 128, 'sprites/hexagon-green.png' ]
-  discard   = [ discard_x, discard_y, 128, 128, 'sprites/hexagon-black.png' ]
-  discard_label = [ discard_count_x, discard_count_y, "Discarded: #{args.state.discardCount}", 0, 0, 0, args.state.discard_label_alpha ]
-  args.outputs.labels << discard_label
+
+
   args.outputs.sprites << red_card
   args.outputs.sprites << green_card
-  args.outputs.sprites << discard
+
 
   if args.inputs.mouse.click
     args.state.last_mouse_click = args.inputs.mouse.click
