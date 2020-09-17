c3bb61602f9b39cfa5453ba2ce94d5300e317621
Matthew <matthewlf98@gmail.com>
2020-09-17 13:29:11 -0500

    Moved player info, enemy info, and discard info into separate functions to be called by the ui

--- app/main.rb
+++ app/main.rb
@@ -1,32 +1,79 @@
+#Player stats array
+def player args
+  args.state.energy ||= 3
+  args.state.player_health ||= 100
+end
+
+def enemy args
+  args.state.enemy_health ||= 100
+end
+
+def discard args
+  args.state.discard_count ||= 0
+end
+
+# def deck args
+#   player(args)
+#   #args.state.deck = [ :attack, :attack, :attack, :attack, :attack,
+#   #                    :heal, :heal, :heal, :heal, :heal ]
+#   args.state.deck = ["attack", "heal"]
+#   args.state.deck_count ||= args.state.deck.length()
+#
+#   # cards = {
+#   #     attack: {
+#   #         sprite: 'sprites/hexagon-red.png',
+#   #         title: 'Attack',
+#   #         description: 'Attack the Enemy for 5 damage',
+#   #         cost: 1,
+#   #         action: -> (args.state.enemy_health) {
+#   #           args.state.enemy_health -= 5
+#   #         }
+#   #     },
+#   #     heal: {
+#   #         sprite: 'sprites/hexagon-green.png',
+#   #         title: 'Heal',
+#   #         description: 'Heal yourself for 7 health',
+#   #         cost: 1,
+#   #         action: -> (args.state.player_health) {
+#   #           args.state.player_health += 7
+#   #         }
+#   #     }
+#   # }
+# end
+
 def ui args
+  player(args)
+  enemy(args)
+  discard(args)
+  #deck(args)
+  args.state.deck = ["attack", "heal"]
+  args.state.deck_count ||= args.state.deck.length()
   #Deck information
   args.state.deck_x ||= 20
   args.state.deck_y ||= 40
-  deck = [ args.state.deck_x, args.state.deck_y, 64, 128, 'sprites/hexagon-indigo.png' ]
-  args.outputs.sprites << deck
+  deck_symbol = [ args.state.deck_x, args.state.deck_y, 64, 128, 'sprites/hexagon-indigo.png' ]
+  args.outputs.sprites << deck_symbol
 
-  args.state.deck_count ||= 30
   args.state.deck_label_alpha ||= 0
   args.state.deck_count_x = args.state.deck_x
   args.state.deck_count_y = args.state.deck_y + 150
   deck_label = [ args.state.deck_count_x, args.state.deck_count_y, "Cards in Deck: #{args.state.deck_count}", 0, 0, 0, args.state.deck_label_alpha ]
   args.outputs.labels << deck_label
 
-  if args.inputs.mouse.point.inside_rect? deck
+  if args.inputs.mouse.point.inside_rect? deck_symbol
     args.state.deck_label_alpha = 255
   end
-  if !args.inputs.mouse.point.inside_rect? deck
+  if !args.inputs.mouse.point.inside_rect? deck_symbol
     args.state.deck_label_alpha = 0
   end
 
 
-  #Discard information
+  #Discard Display
   args.state.discard_x ||= 1150
   args.state.discard_y ||= 40
   discard = [ args.state.discard_x, args.state.discard_y, 64, 128, 'sprites/hexagon-black.png' ]
   args.outputs.sprites << discard
 
-  args.state.discard_count ||= 0
   args.state.discard_label_alpha ||= 0
   args.state.discard_count_x = args.state.discard_x
   args.state.discard_count_y = args.state.discard_y + 150
@@ -41,18 +88,23 @@ def ui args
   end
 
 
-  #Energy information (resource is a WIP name)
-  args.state.resource ||= 3
-  args.outputs.labels << [ 25, 650, "Energy: #{args.state.resource}" ]
+  #Energy display
+  args.outputs.labels << [ 25, 650, "Energy: #{args.state.energy}" ]
 
-  #Player information
+  #Player display
   args.state.player_x ||= 250
   args.state.player_y ||= 400
   player = [ args.state.player_x, args.state.player_y, 100, 100,  'sprites/dragon-0.png' ]
   args.outputs.sprites << player
 
-  args.state.player_health ||= 100
   args.state.player_health_x ||= args.state.player_x
+  args.state.player_health_y ||= args.state.player_y + 150
+  args.outputs.labels << [ args.state.player_health_x, args.state.player_health_y, "Your Health: #{args.state.player_health}" ]
+
+  #Enemy Display
+  args.state.enemy_x ||= 750
+  args.state.enemy_y ||= 400
+  args.state.enemy_health_x ||= args.state.player_x
   args.state.player_health_y ||= args.state.player_y + 150
   args.outputs.labels << [ args.state.player_health_x, args.state.player_health_y, "Your Health: #{args.state.player_health}" ]
 
@@ -73,8 +125,8 @@ def ui args
 
   if args.state.last_mouse_click
     if args.state.last_mouse_click.point.inside_rect? red_card
-      if(args.state.resource != 0)
-        args.state.resource -= 1
+      if(args.state.energy != 0)
+        args.state.energy -= 1
         args.state.health -= 5
         args.state.last_mouse_click = nil
         args.state.discard_count += 1
@@ -87,6 +139,9 @@ def ui args
 end
 
 def tick args
+  player(args)
+  enemy(args)
+  # deck(args)
   ui(args)
 end
 
